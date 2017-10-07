36109ec9cc22b32fc90b96dc83e0f205e22749e9
Bernat Gabor <bgabor8@bloomberg.net>
2017-10-07 20:23:53 +0100

    add voice call

--- ui-flask/main.py
+++ ui-flask/main.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 from datetime import datetime
@@ -7,6 +8,7 @@ from flask import Flask, request
 from flask import send_from_directory, render_template
 from twilio.rest import Client
 from twilio.twiml.messaging_response import MessagingResponse
+from twilio.twiml.voice_response import Gather, VoiceResponse
 
 TWILIO_ACCOUNT = os.getenv('TWILIO_ACCOUNT')
 TWILIO_AUTH = os.getenv('TWILIO_AUTH')
@@ -76,12 +78,18 @@ def hello():
 
 @app.route('/sms', methods=['POST'])
 def sms():
+    msg, to, uuid = get_contact_user()
+    SMS_HISTORY[to] = uuid
+    message = TWILIO_CLIENT.messages.create(to=to, from_=TWILIO_FROM_PHONE, body=msg)
+    call_id = voice_call()
+    return json.dumps({'message': message.sid, 'call': call_id})
+
+
+def get_contact_user():
     uuid, to, msg = request.form['uuid'], request.form['to'], request.form['msg']
     if to.startswith('00'):
         to = '+{}'.format(to[2:])
-    SMS_HISTORY[to] = uuid
-    message = TWILIO_CLIENT.messages.create(to=to, from_=TWILIO_FROM_PHONE, body=msg)
-    return message.sid
+    return msg, to, uuid
 
 
 @app.route("/sms_respond", methods=['POST'])
@@ -92,6 +100,7 @@ def sms_reply():
     app.logger.info('got response {} {} {}'.format(uuid, from_, body))
 
     if uuid is not None and body:
+        msg, to, uuid = get_contact_user()
         requests.post('http://localhost:8888', data={'uuid': uuid,
                                                      "old_state": 'to_acknowledge',
                                                      "new_state": 'in_progress'})
@@ -101,6 +110,44 @@ def sms_reply():
         return str(resp)
 
     return None
+
+
+MSG_STORE = {}
+
+
+@app.route("/voice_respond", methods=['POST'])
+def voice_call():
+    msg, to, uuid = get_contact_user()
+    MSG_STORE[uuid, to] = msg
+    call = TWILIO_CLIENT.calls.create(
+        to=to,
+        from_=TWILIO_FROM_PHONE,
+        url="{}/voice_handle?uuid={}&to{}".format('https://precocial-tang-6014.dataplicity.io/', uuid, to)
+    )
+    return call.sid
+
+
+@app.route("/voice_handle", methods=['GET'])
+def voice_handle(uuid, to):
+    msg = MSG_STORE[uuid, to]
+    resp = VoiceResponse()
+
+    if 'Digits' in request.values:
+        choice = request.values['Digits']
+        if choice == '1':
+            resp.say('Alert accepted!')
+            return str(resp)
+        elif choice == '2':
+            resp.say('Try again!')
+            return str(resp)
+        else:
+            # If the caller didn't choose 1 or 2, apologize and ask them again
+            resp.say("Sorry, I don't understand that choice.")
+    gather = Gather(num_digits=1)
+    gather.say(msg)
+    resp.append(gather)
+    resp.redirect('/voice')
+    return str(resp)
 
 
 @app.errorhandler(500)
